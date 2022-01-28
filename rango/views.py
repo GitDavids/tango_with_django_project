33a4717c3ca339673245188935fa496137788181
Davids Zakrevskis <2577947z@student.gla.ac.uk>
2022-01-28 12:53:14 +0000

    Test 4, passed

--- rango/views.py
+++ rango/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 def index(request):
     context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
-    return render(request, 'rango/index.html', context=context_dict) # (<a href='/rango/about/'>About</a>
+    return render(request, 'rango/index.html', context=context_dict) # <a href='/rango/about/'>About</a>
 
 def about(request):
     return render(request, 'rango/about.html') # <a href='/rango/'>Index</a>
